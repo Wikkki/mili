79d052aff4797854a201653aa3d763333781ac04
Viktorija Gorbunova <vika_gorbunova_2014@mail.ru>
2022-12-11 12:49:00 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+       int  price = 2000;
+       int OneMile = 20;
+
+
+        int bonus = price / OneMile;
+
+      System.out.println("Начислено " + bonus + " бонусных миль " );
+    }
+
+}
