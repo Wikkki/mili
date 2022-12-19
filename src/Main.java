6ea17eae9fcb8e8dde9d23764b7eac89b60997e4
Wikkki <108623629+Wikkki@users.noreply.github.com>
2022-12-19 21:23:53 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-       int  price = 2000;
-       int OneMile = 20;
+        intprice = 2000;
+        int OneMile = 20;
 
 
         int bonus = price / OneMile;
 
-      System.out.println("Начислено " + bonus + " бонусных миль " );
+        System.out.println("Начислено " + bonus + " бонусных миль ");
     }
 
 }
